3f6425e83f547e5010ee40a84eb46a3937b88c56
Glenn Strauss <gstrauss@gluelogic.com>
2026-05-21 13:42:57 -0400

    update to umami.is analytics
    host analytics js with local site:
    host https://cloud.umami.is/script.js at /analytics.js
    reference:
    https://docs.umami.is/docs/bypass-ad-blockers#hosting-the-tracker-script
    
    Respect user's Do Not Track browser setting w/ data-do-not-track="true"

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,5 @@
 module.exports = {
-  author: "April King",
-  contentSecurityPolicy: "default-src 'none'; base-uri 'none'; connect-src 'self' https://www.google-analytics.com; font-src 'self' https://code.cdn.mozilla.net; img-src 'self' https://www.google-analytics.com; script-src 'self' https://www.google-analytics.com/analytics.js https://www.googletagmanager.com/gtag/js; style-src 'self' https://code.cdn.mozilla.net",
+  contentSecurityPolicy: "default-src 'none'; base-uri 'none'; connect-src 'self' https://api-gateway.umami.dev https://data.tlsref.org; font-src 'self' https://code.cdn.mozilla.net; img-src 'self'; script-src 'self'; style-src 'self' https://code.cdn.mozilla.net",
   description: "An easy-to-use secure configuration generator for web, database, and mail software. Simply select the software you are using and receive a configuration file that is both safe and compatible.",
   header: "TLS Configurator",
   localContentSecurityPolicy: "default-src * 'unsafe-inline'",  // supports autoreload
